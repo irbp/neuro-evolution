d5cf11bf13f3c7daf5afb25ff084f771fe8d676c
Ítalo Paulino <irbp@cin.ufpe.br>
2018-11-06 15:05:42 -0300

    Getting epochs from parameters

--- neuro_evolution/train.py
+++ neuro_evolution/train.py
@@ -79,7 +79,7 @@ def train_and_score(network, x_train, y_train, x_test, y_test):
 
     model.fit(x_train, y_train,
               batch_size=network.get('batch_size', 128),
-              epochs=10000,  # using early stopping, so no real limit
+              epochs=network.get('epochs', 10000),  # using early stopping, so no real limit
               verbose=network.get('verbose', 0),
               validation_data=(x_test, y_test),
               callbacks=[early_stopper])
